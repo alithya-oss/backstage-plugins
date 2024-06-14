e4dea2af4a43c62155256a4d65581c0b1b63f684
Florian JUDITH <florian.judith.b@gmail.com>
2024-06-13 23:12:03 -0400

    Remove uuid-ossp extension check

--- workspaces/time-saver/plugins/time-saver-backend/migrations/init.js
+++ workspaces/time-saver/plugins/time-saver-backend/migrations/init.js
@@ -20,115 +20,36 @@
  * @param {import('knex').Knex} knex
  */
 exports.up = async function up(knex) {
-  let namespace = '';
-  let response = {};
-  const isPostgreSQL = knex.client.config.client === 'pg';
-
-  // create extension only for postgreSQL
-  if (isPostgreSQL) {
-    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"').then(
-      () => {
-        response = {
-          ...response,
-          pg_extension: 'enabled',
-        };
-        console.log('uuid-ossp extension enabled');
-      },
-      reason => {
-        response = {
-          ...response,
-          pg_extension: `disabled: ${reason}}`,
-        };
-        console.log(`Could not create uuid-ossp extension: ${reason}`);
-      },
-    );
-
-    await knex
-      .raw(
-        `SELECT (select nspname from pg_catalog.pg_namespace where oid=extnamespace)
-      FROM pg_extension where extname='uuid-ossp';`,
-      )
-      .then(
-        s => {
-          namespace = s.rows[0].nspname;
-          response = {
-            ...response,
-            namespace,
-          };
-          console.log(`uuid-ossp extension created in ${namespace} schema`);
-        },
-        reason => {
-          response = {
-            ...response,
-            namespace: `undefined: ${reason}`,
-          };
-          console.log('uuid-ossp extension was not found.');
-        },
-      );
-  }
-
-  await knex.schema
-    .createTable('ts_template_time_savings', table => {
-      table.comment(
-        'Table contains template time savings with relation to the templateTaskId',
-      );
-      table
-        .uuid('id')
-        .primary()
-        .defaultTo(
-          isPostgreSQL
-            ? knex.raw(`${namespace}.uuid_generate_v4()::uuid`)
-            : knex.fn.uuid(),
-        )
-        .comment('UUID');
-
-      table
-        .timestamp('created_at', { useTz: false, precision: 0 })
-        .notNullable()
-        .defaultTo(knex.fn.now())
-        .comment('The creation time of the record');
-
-      table.string('template_task_id').comment('Template task ID');
-
-      table
-        .string('template_name')
-        .comment('Template name as template entity_reference');
-
-      table.string('team').comment('Team name of saved time');
-
-      table
-        .float('time_saved', 2)
-        .comment('time saved by the team within template task ID, in hours');
-
-      table.string('template_task_status').comment('template task status');
-
-      table
-        .string('created_by')
-        .comment('entity reference to the user that has executed the template');
-    })
-    .then(
-      s => {
-        response = {
-          ...response,
-          ts_template_time_savings: s,
-        };
-        console.log('Created table ts_template_time_savings.');
-      },
-      reason => {
-        response = {
-          ...response,
-          ts_template_time_savings: `Not created: ${reason}`,
-        };
-        console.log('Failed to create table ts_template_time_savings.');
-      },
-    );
-
-  return response;
-};
+  await knex.schema.createTable('ts_template_time_savings', table => {
+    table.comment('Table contains template time savings with relation to the templateTaskId');
+    table
+      .bigIncrements('index')
+      .notNullable()
+      .comment('An insert counter to ensure ordering');
+    table.uuid('id').notNullable().comment('The ID of the time saver result');
+    table
+      .dateTime('created_at')
+      .defaultTo(knex.fn.now())
+      .notNullable()
+      .comment('The timestamp when this entry was created');
+    table.string('template_task_id').comment('The template task ID');
+    table
+      .string('template_name')
+      .comment('The template name used as template entity_reference');
+    table.string('team').comment('The team name of saved time');
+    table
+      .float('time_saved', 2)
+      .comment('The time saved by the team within template task ID, in hours');
+    table.string('template_task_status').comment('The template task status');
+    table
+      .string('created_by')
+      .comment('the entity reference related to the user who launched the template');
+    });
+  };
 
 /**
  * @param {import('knex').Knex} knex
  */
 exports.down = async function down(knex) {
-  return knex.schema.dropTable('ts_template_time_savings');
+  await knex.schema.dropTable('ts_template_time_savings');
 };
