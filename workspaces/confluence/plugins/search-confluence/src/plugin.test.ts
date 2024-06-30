f6d71937cdccc69e67a3006c546d0e58445f3e77
Florian JUDITH <florian.judith.b@gmail.com>
2024-06-30 13:05:20 -0400

    Updated confluence plugin

--- workspaces/confluence/plugins/search-confluence/src/plugin.test.ts
+++
@@ -1,7 +0,0 @@
-import { searchConfluenceFrontendPlugin } from './plugin';
-
-describe('search-confluence', () => {
-  it('should export search-confluence Plugin', () => {
-    expect(searchConfluenceFrontendPlugin).toBeDefined();
-  });
-});
--- workspaces/confluence/plugins/search-confluence/src/plugin.ts
+++
@@ -1,5 +0,0 @@
-import { createPlugin } from '@backstage/core-plugin-api';
-
-export const searchConfluenceFrontendPlugin = createPlugin({
-  id: 'search-confluence-frontend',
-});
