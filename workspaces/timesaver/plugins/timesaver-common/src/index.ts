d0ab431ee66684057051d1f75c893a8675bb01b3
Javier Prieto <javier.accounts@fiml.co>
2024-07-08 15:41:17 -0400

    feat: create basic timesaver common plugin read permission

--- workspaces/timesaver/plugins/timesaver-common/src/index.ts
+++ workspaces/timesaver/plugins/timesaver-common/src/index.ts
@@ -1,19 +1,23 @@
-/***/
-/**
- * Common functionalities for the timesaver plugin.
+/*
+ * Copyright 2024 The Backstage Authors
  *
- * @packageDocumentation
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+import { createPermission } from '@backstage/plugin-permission-common';
 
-/**
- * In this package you might for example declare types that are common
- * between the frontend and backend plugin packages.
- */
-export type CommonType = {
-  field: string;
-};
+export const timeSaverPermission = createPermission({
+  name: 'timeSaver',
+  attributes: { action: 'read' },
+});
 
-/**
- * Or you might declare some common constants.
- */
-export const COMMON_CONSTANT = 1;
+export const timeSaverPermissions = [timeSaverPermission];
