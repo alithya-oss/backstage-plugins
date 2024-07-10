40d5cd4ac000cadedafc4fe0dfffdb0326a9ca6b
Javier Prieto <javier.accounts@fiml.co>
2024-07-10 11:32:35 -0400

    feat: add static routes to Time Saver backend plug-in

--- workspaces/timesaver/plugins/timesaver-backend/src/service/router.ts
+++ workspaces/timesaver/plugins/timesaver-backend/src/service/router.ts
@@ -23,30 +23,115 @@ export async function createRouter(
   });
 
   //  Get all groups
+  router.get('/groups', async (_, response) => {
+    return response.json({
+      msg: 'groups',
+      status: 'ok',
+    });
+  });
 
   //  Get all templates
+  router.get('/templates', async (_, response) => {
+    return response.json({
+      msg: 'templates',
+      status: 'ok',
+    });
+  });
 
   //  Get all templates tasks
+  router.get('/templatesTasks', async (_, response) => {
+    return response.json({
+      msg: 'templatesTasks',
+      status: 'ok',
+    });
+  });
 
   //  Get all templates count
+  router.get('/templatesCount', async (_, response) => {
+    return response.json({
+      msg: 'templatesCount',
+      status: 'ok',
+    });
+  });
 
   //  Get time saved sum
+  router.get('/timeSavedSum', async (_, response) => {
+    return response.json({
+      msg: 'timeSavedSum',
+      status: 'ok',
+    });
+  });
 
   //  Get savings
+  router.get('/savings', async (_, response) => {
+    return response.json({
+      msg: 'savings',
+      status: 'ok',
+    });
+  });
 
   //  Get statistics
-
-  //  Get stats per group
+  router.get('/statistics', async (request, response) => {
+    const { groupId } = request.query;
+    if (groupId) {
+      return response.json({
+        msg: `/statistics?groupId=${groupId}`,
+        status: 'ok',
+      });
+    }
+    return response.json({
+      msg: `statistics`,
+      status: 'ok',
+    });
+  });
 
   //  Get daily time summary per team
-
-  //  Get daily time summary per template
+  router.get('/dailyTimeSum', async (request, response) => {
+    const { teamId, templateId } = request.query;
+    if (teamId) {
+      return response.json({
+        msg: `/dailyTimeSum?teamId=${teamId}`,
+        status: 'ok',
+      });
+    } else if (templateId) {
+      return response.json({
+        msg: `/dailyTimeSum?templateId=${templateId}`,
+        status: 'ok',
+      });
+    }
+    return response.json({
+      msg: 'dailyTimeSum',
+      status: 'ok',
+    });
+  });
 
   //  Get time summary per team
-
-  //  Get time summary per template
+  router.get('/timeSum', async (request, response) => {
+    const { teamId, templateId } = request.query;
+    if (teamId) {
+      return response.json({
+        msg: `/timeSum?teamId=${teamId}`,
+        status: 'ok',
+      });
+    } else if (templateId) {
+      return response.json({
+        msg: `/timeSum?templateId=${templateId}`,
+        status: 'ok',
+      });
+    }
+    return response.json({
+      msg: 'timeSum',
+      status: 'ok',
+    });
+  });
 
   //  Execute migration
+  router.get('/migrateTemplatesTasks', async (_, response) => {
+    return response.json({
+      msg: '/migrateTemplatesTasks',
+      status: 'ok',
+    });
+  });
 
   const middleware = MiddlewareFactory.create({ logger, config });
 
