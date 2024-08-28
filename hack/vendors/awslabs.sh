5d6f6cf4810a31f0ed559a1535bfa3530b001243
Florian JUDITH <florian.judith.b@gmail.com>
2024-08-28 11:51:39 -0400

    Initial Harmonix fork

--- hack/vendors/awslabs.sh
+++ hack/vendors/awslabs.sh
@@ -3,84 +3,84 @@ set -eEuo pipefail
 
 VERSION="main"
 
-function sync() {
-    mkdir -p .tmp/backstage-plugins-for-aws/${VERSION}
+function sync_aws_core() {
+    mkdir -p .tmp/harmonix/${VERSION}
 
-    curl -fsSL https://github.com/awslabs/backstage-plugins-for-aws/archive/refs/heads/${VERSION}.tar.gz |
+    curl -fsSL https://github.com/awslabs/harmonix/archive/refs/heads/${VERSION}.tar.gz |
         tar -xvzf - \
             --strip-components=1 \
-            --directory=.tmp/backstage-plugins-for-aws/${VERSION} \
-            backstage-plugins-for-aws-${VERSION/"/"/"-"}/plugins
+            --directory=.tmp/harmonix/${VERSION} \
+            harmonix-${VERSION/"/"/"-"}/plugins
 
     # codebuild
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codebuild/backend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codebuild/backend/* \
         ./workspaces/aws/plugins/aws-codebuild-backend
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codebuild/common/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codebuild/common/* \
         ./workspaces/aws/plugins/aws-codebuild-common
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codebuild/frontend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codebuild/frontend/* \
         ./workspaces/aws/plugins/aws-codebuild
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codebuild/README.md \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codebuild/README.md \
         ./workspaces/aws/plugins/aws-codebuild.md
 
     # codepipeline
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codepipeline/backend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codepipeline/backend/* \
         ./workspaces/aws/plugins/aws-codepipeline-backend
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codepipeline/common/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codepipeline/common/* \
         ./workspaces/aws/plugins/aws-codepipeline-common
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codepipeline/frontend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codepipeline/frontend/* \
         ./workspaces/aws/plugins/aws-codepipeline
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/codepipeline/README.md \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/codepipeline/README.md \
         ./workspaces/aws/plugins/aws-codepipeline.md
 
     # aws-core
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/core/common/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/core/common/* \
         ./workspaces/aws/plugins/aws-core-common
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/core/react/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/core/react/* \
         ./workspaces/aws/plugins/aws-core-react
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/core/scaffolder-actions/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/core/scaffolder-actions/* \
         ./workspaces/aws/plugins/scaffolder-backend-module-aws-core-actions
 
     # cost-insights
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/cost-insights/backend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/cost-insights/backend/* \
         ./workspaces/aws/plugins/aws-cost-insights-backend
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/cost-insights/common/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/cost-insights/common/* \
         ./workspaces/aws/plugins/aws-cost-insights-common
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/cost-insights/frontend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/cost-insights/frontend/* \
         ./workspaces/aws/plugins/aws-cost-insights
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/cost-insights/README.md \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/cost-insights/README.md \
         ./workspaces/aws/plugins/aws-cost-insights.md
 
     # ecs
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/ecs/backend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/ecs/backend/* \
         ./workspaces/aws/plugins/amazon-ecs-backend
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/ecs/common/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/ecs/common/* \
         ./workspaces/aws/plugins/amazon-ecs-common
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/ecs/frontend/* \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/ecs/frontend/* \
         ./workspaces/aws/plugins/amazon-ecs
     rsync -av \
-        .tmp/backstage-plugins-for-aws/${VERSION/"/"/"-"}/plugins/ecs/README.md \
+        .tmp/harmonix/${VERSION/"/"/"-"}/plugins/ecs/README.md \
         ./workspaces/aws/plugins/amazon-ecs.md
 }
 
-function rename() {
+function rename_aws_core() {
     changes=(
         "s#@aws/aws-core-plugin-for-backstage-scaffolder-actions#@alithya-oss/scaffolder-backend-module-aws-core-actions#g"
         "s#@aws/amazon-ecs-plugin-for-backstage#@alithya-oss/plugin-amazon-ecs#g"
@@ -103,8 +103,54 @@ function rename() {
     done
 }
 
-sync
+function sync_harmonix() {
+    mkdir -p .tmp/harmonix/${VERSION}
+
+    curl -fsSL https://github.com/awslabs/harmonix/archive/refs/heads/${VERSION}.tar.gz |
+        tar -xvzf - \
+            --strip-components=1 \
+            --directory=.tmp/harmonix/${VERSION} \
+            harmonix-${VERSION/"/"/"-"}/backstage-plugins/plugins
+
+    rsync -av \
+        .tmp/harmonix/${VERSION/"/"/"-"}/backstage-plugins/plugins/aws-apps-backend/* \
+        ./workspaces/aws/plugins/aws-apps-backend
+    rsync -av \
+        .tmp/harmonix/${VERSION/"/"/"-"}/backstage-plugins/plugins/aws-apps-common/* \
+        ./workspaces/aws/plugins/aws-apps-commont
+    rsync -av \
+        .tmp/harmonix/${VERSION/"/"/"-"}/backstage-plugins/plugins/aws-apps/* \
+        ./workspaces/aws/plugins/aws-apps
+    rsync -av \
+        .tmp/harmonix/${VERSION/"/"/"-"}/backstage-plugins/plugins/catalog-backend-module-aws-apps-entities-processor \
+        ./workspaces/aws/plugins/catalog-backend-module-aws-apps-entities-processor
+    rsync -av \
+        .tmp/harmonix/${VERSION/"/"/"-"}/backstage-plugins/plugins/scaffolder-backend-module-aws-apps \
+        ./workspaces/aws/plugins/scaffolder-backend-module-aws-apps
+}
+
+function rename_harmonix() {
+    changes=(
+        "s#@aws/#@alithya-oss/#g"
+    )
+
+    for item in "${changes[@]}"; do
+    find -type f \
+        -not -name "install-state.gz" \
+        -not -name "yarn.lock" \
+        -not -name "CHANGELOG.md" \
+        -not -path "**/.yarn*" \
+        -not -path "**/.changeset" \
+        -not -path "**/node_modules*" \
+        -not -path "**/dist*" \
+        -exec sed -i ${item} {} +
+    done
+}
+
+# sync_aws_core
+sync_harmonix
 cd ./workspaces/aws/
-rename
-yarn install
-yarn backstage-cli versions:bump --release 1.30.2
+# rename_aws_core
+rename_harmonix
+# yarn install
+# yarn backstage-cli versions:bump --release 1.30.2
---
+++ workspaces/aws/plugins/aws-apps-commont/CHANGELOG.md
@@ -0,0 +1,5 @@
+# @aws/plugin-aws-apps-common-for-backstage
+
+## 0.1.0
+
+Initial release
