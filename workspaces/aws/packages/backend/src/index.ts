0bb29c454647dd4d82e73bfa40fb09b24719ebbb
Nizar Ajroud <nizar.ajroud@gmail.com>
2024-09-11 00:29:16 +0000

    add related changes

--- workspaces/aws/packages/backend/src/index.ts
+++ workspaces/aws/packages/backend/src/index.ts
@@ -74,4 +74,5 @@ backend.add(import('./extensions/scaffolderActions'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github/alpha'));
 backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
+backend.add(import('@alithya-oss/plugin-catalog-backend-module-aws-apps-entities-processor'));
 backend.start();
