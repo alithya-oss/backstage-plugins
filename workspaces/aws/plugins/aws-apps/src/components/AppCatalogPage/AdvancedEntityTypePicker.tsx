c424b0d91c7e5e71212d240ec95e0800cfbce958
Frédéric Léveillée <fleveillee@gmail.com>
2024-10-07 19:21:13 -0400

    lint fixes

--- workspaces/aws/plugins/aws-apps/src/components/AppCatalogPage/AdvancedEntityTypePicker.tsx
+++ workspaces/aws/plugins/aws-apps/src/components/AppCatalogPage/AdvancedEntityTypePicker.tsx
@@ -7,10 +7,7 @@ import { Box } from '@material-ui/core';
 
 import { alertApiRef, useApi } from '@backstage/core-plugin-api';
 import { Select } from '@backstage/core-components';
-import {
-  EntityTypePickerProps,
-  useEntityTypeFilter,
-} from '@backstage/plugin-catalog-react';
+import { EntityTypePickerProps, useEntityTypeFilter } from '@backstage/plugin-catalog-react'
 
 function filterTypes(
   allTypes: string[],
@@ -39,15 +36,13 @@ function filterTypes(
     }
     return 0;
   });
-  const typesMap = availableTypes.reduce(
+  return availableTypes.reduce(
     (acc, kind) => {
       acc[kind.toLocaleLowerCase('en-US')] = kind;
       return acc;
     },
     {} as Record<string, string>,
   );
-
-  return typesMap;
 }
 
 /**
