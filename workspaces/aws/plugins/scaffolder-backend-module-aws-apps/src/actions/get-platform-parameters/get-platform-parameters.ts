3ec4b97d506aeab164871499aaeed49a47a16d3d
Frédéric Léveillée <fleveillee@gmail.com>
2024-10-08 09:29:52 -0400

    lint fixes

--- workspaces/aws/plugins/scaffolder-backend-module-aws-apps/src/actions/get-platform-parameters/get-platform-parameters.ts
+++ workspaces/aws/plugins/scaffolder-backend-module-aws-apps/src/actions/get-platform-parameters/get-platform-parameters.ts
@@ -104,7 +104,7 @@ export function getPlatformParametersAction(options: { envConfig: Config }) {
       const getEnvProviderSsmParams = async (): Promise<{
         [key: string]: string;
       }> => {
-        const params = (
+        return (
           await Promise.all(
             paramKeys.map(
               async (paramKey): Promise<{ [key: string]: string }> => {
@@ -127,8 +127,6 @@ export function getPlatformParametersAction(options: { envConfig: Config }) {
             [key]: paramKeyValMap[key],
           };
         }, {});
-
-        return params;
       };
       const envParams = await getEnvProviderSsmParams();
       ctx.logger.info(JSON.stringify(envParams));
