2a221b8600b788864f2c2259a85493b4c48bf5e6
Frédéric Léveillée <fleveillee@gmail.com>
2024-10-28 15:20:33 -0400

    POSIX compliant docker compose stop

--- workspaces/aws/hack/run.sh
+++ workspaces/aws/hack/run.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+
+cleanup(){
+  docker compose stop
+}
+
+# Trap the SIGINT signal (Ctrl+C)
+trap cleanup SIGINT
+
 source hack/init.sh
 
 docker compose -f docker-compose.yaml up --build --detach --remove-orphans
@@ -15,5 +23,4 @@ done
 
 einfo "Starting Backstage in development mode"
 
-LOG_LEVEL=debug yarn dev \
-|| docker compose stop
+LOG_LEVEL=debug yarn dev
