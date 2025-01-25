ce7e271832111f692ce39232488187f5b2386012
Florian JUDITH <florian.judith@alithya.com>
2025-01-25 02:51:02 -0500

    Added support for Bulletin Board backend

--- workspaces/bulletin-board/packages/backend/src/index.ts
+++ workspaces/bulletin-board/packages/backend/src/index.ts
@@ -52,5 +52,5 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
-backend.add(import('@alithya-oss/backstage-plugin-bulletin-backend'));
+backend.add(import('@alithya-oss/backstage-plugin-bulletin-board-backend'));
 backend.start();
