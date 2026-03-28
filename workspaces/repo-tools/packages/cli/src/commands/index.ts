c6222f3c1ca54c56d6d3556f283d090cf745cc4a
Florian JUDITH <florian.judith@alithya.com>
2026-03-28 18:12:54 -0400

    feat(repo-tools): align with upstream
    Signed-off-by: Florian JUDITH <florian.judith@alithya.com>

--- workspaces/repo-tools/packages/cli/src/commands/index.ts
+++ workspaces/repo-tools/packages/cli/src/commands/index.ts
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { Command } from 'commander';
 import { exitWithError } from '../lib/errors';
 import { assertError } from '@backstage/errors';
